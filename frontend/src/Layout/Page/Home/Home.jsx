fac4148c4857ca1331558da92ee75b835ad6aa8d
Kayoshi <r0n1ronov1@gmail.com>
2023-12-05 17:58:59 +0300

    drag and drop

--- frontend/src/Layout/Page/Home/Home.jsx
+++ frontend/src/Layout/Page/Home/Home.jsx
@@ -1,12 +1,14 @@
 import {DndProvider} from "react-dnd"
 import {HTML5Backend} from "react-dnd-html5-backend"
 import Board from "../../../components/Board/Board";
+import DragDrop from "../../../components/DragDrop";
 
 const Home = () => {
     return(
         <>
             <DndProvider backend={HTML5Backend}>
-                <Board/>
+
+
             </DndProvider>
         </>
     )
