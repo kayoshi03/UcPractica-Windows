987e9ee8427c80e20cfb77dfe714c558c9ba8ae7
task <takevichoff@gmail.com>
2023-12-05 15:32:44 +0500

    Верстка в файл frontend и заготовка JWT авторизации

---
+++ backend/auth.py
@@ -0,0 +1,47 @@
+from fastapi import HTTPException
+from fastapi.security.http import HTTPBase, HTTPBearer, HTTPAuthorizationCredentials
+from fastapi.openapi.models import HTTPBearer as HTTPBearerModel
+from fastapi.security.utils import get_authorization_scheme_param
+from starlette.status import HTTP_403_FORBIDDEN
+from starlette.requests import Request
+
+
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
+SECRET_KEY = "supersecretkey"
+ALGORITHM = "HS256"
+
+
+class CustomHTTPBearer(HTTPBase):
+    def __init__(
+        self,
+        *,
+        bearerFormat: str = None,
+        scheme_name: str = None,
+        description: str = None,
+        auto_error: bool = True,
+    ):
+        self.model = HTTPBearerModel(bearerFormat=bearerFormat, description=description)
+        self.scheme_name = scheme_name or self.__class__.__name__
+        self.auto_error = auto_error
+
+    async def __call__(
+        self, request: Request
+    ) -> HTTPAuthorizationCredentials:
+        authorization = request.headers.get("Authorization") or request.cookies.get("access_token_cookie")
+        scheme, credentials = get_authorization_scheme_param(authorization)
+        if not (authorization and scheme and credentials):
+            if self.auto_error:
+                raise HTTPException(
+                    status_code=HTTP_403_FORBIDDEN, detail="Not authenticated"
+                )
+            else:
+                return None
+        if scheme.lower() != "bearer":
+            if self.auto_error:
+                raise HTTPException(
+                    status_code=HTTP_403_FORBIDDEN,
+                    detail="Invalid authentication credentials",
+                )
+            else:
+                return None
+        return HTTPAuthorizationCredentials(scheme=scheme, credentials=credentials)
--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,24 @@
 import requests
 
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
 from schemas import DefaultResponse
 from models import *
+from auth import *
 
 
 app = FastAPI()
+
+security = CustomHTTPBearer()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/", response_model=DefaultResponse, tags=["API", "HEAD"])
