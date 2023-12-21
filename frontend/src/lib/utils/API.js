540fa8991078d26839c76663a60efb5aac5b725f
Kayoshi <r0n1ronov1@gmail.com>
2023-12-21 14:54:03 +0300

    новый апи

--- frontend/src/lib/utils/API.js
+++ frontend/src/lib/utils/API.js
@@ -8,7 +8,7 @@ export const is_auth = () => {
 }
 
 export const API = axios.create({
-    baseURL: "http://31.129.105.229:8080",
+    baseURL: "http://31.129.99.15:8080",
     headers: {
         Authorization: `Bearer ${cookie.get("access_token_cookie")}`,
         "Access-Control-Allow-Origin" : "*"
